14cd3ee5a3701c056c761974e61047f87b5fb3ad
Kehase Teklay <fetawiseb2@gmail.com>
2025-11-08 18:50:57 +0100

    correction to my logging code

--- app/core/logging.py
+++ app/core/logging.py
@@ -58,11 +58,11 @@ def get_logger() -> None:
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
 
-    # stream_handler = logging.StreamHandler()
-    # stream_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
-    # logger.addHandler(stream_handler)
+    logger.addHandler(stream_handler)
 
     return logger
 
